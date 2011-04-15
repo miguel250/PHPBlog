205fd2a63278c0ea2a319711159b9fdfbefd2eb0
Miguel Perez <miguel250@gmail.com>
2011-04-14 23:07:46 -0400

    added functions to check for home and 404 error

--- inc/Handler.php
+++ inc/Handler.php
@@ -9,6 +9,7 @@
 class Handler {
 
     private $url;
+    public   $notfound;
 
     /*     * *
      * Constructor  if the $url variable is / then display home page else get post
@@ -19,13 +20,29 @@ class Handler {
     function __construct() {
         $url = $_SERVER['REQUEST_URI'];
         $this->url = $url;
-        
+          $notfound = FALSE;
+          $this->notfound = $notfound;
     }
 
-    /*     * *
-     * Set post url 
-     * @return String $url
+    /**
+     * check in user is in the home page
+     * @return bool 
      */
+    function home() {
+        if ($this->url == '/') {
+            return $this->home = TRUE;
+        } else {
+            $this->home = FALSE;
+        }
+    }
+ function notfound(){ 
+     header("HTTP/1.0 404 Not Found");;
+      $this->notfound = True;
+ }
+   /**
+    *Set url called
+    * @return string url
+    */
 
     function setUrl() {
         return $this->url;
